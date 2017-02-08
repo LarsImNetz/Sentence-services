e6eca888e4dd8eae6f0bfc67687462069c94d739
Lars Langhans <lars.langhans@hypoport.de>
2017-02-08 12:32:30 +0100

    verwendet den Uppercase Decorator

--- src/main/java/org/lla_private/service/kyrillisch/BuchstabenImSatzKyrillischService.java
+++ src/main/java/org/lla_private/service/kyrillisch/BuchstabenImSatzKyrillischService.java
@@ -1,16 +1,16 @@
 package org.lla_private.service.kyrillisch;
 
 import text.manipulation.decorator.NoUmlautDecorator;
+import text.manipulation.decorator.UppercaseDecorator;
 import text.manipulation.satz.SatzManipulator;
 import text.manipulation.word.BuchstabenToKyrillisch;
 
 public class BuchstabenImSatzKyrillischService implements IBuchstabenImSatzKyrillischService {
 
-	// TODO: wir brauchen doch einen UpperCaseDecorator, damit die Reihenfolge eingehalten wird
 	@Override
 	public String convert(String satz) {
-		SatzManipulator satzManipulator = new SatzManipulator(new NoUmlautDecorator(new BuchstabenToKyrillisch()));
-		satzManipulator.setSentence(satz.toUpperCase());
+		SatzManipulator satzManipulator = new SatzManipulator(new NoUmlautDecorator(new UppercaseDecorator(new BuchstabenToKyrillisch())));
+		satzManipulator.setSentence(satz);
 		return satzManipulator.getSentence();
 	}
 
